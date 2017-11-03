1e186cd67676547a1c87f97cc429bb576f133c32
lbb <l>
2017-11-03 14:23:02 +0800

    移动端分类页完成

--- public/front/js/category.js
+++ public/front/js/category.js
@@ -1,3 +1,35 @@
-/**
- * Created by dell on 2017-11-1.
- */
+//开启左侧的区域滚动
+var sc = mui('.mui-scroll-wrapper').scroll({
+  deceleration: 0.0005,
+    indicators: false
+});
+//渲染一级分类
+$.ajax({
+  type: "get",
+  url: "/category/queryTopCategory",
+  success: function (data) {
+    $(".lt_category_l ul").html(template("tpl", data));
+    renderSecond(data.rows[0].id);
+  }
+});
+//渲染一级分类相对应的二级分类
+function renderSecond(id) {
+  $.ajax({
+    type: "get",
+    url:"/category/querySecondCategory",
+    data:{
+      id:id
+    },
+    success:function (data) {
+      // console.log(data);
+      $(".lt_category_r ul").html(template("tpl2",data))
+    }
+  })
+}
+//点击左边的一级分类渲染相对应的二级分类
+$(".lt_category_l").on("click","li",function () {
+  //点击li就让他添加now 其他的兄弟移除now
+  $(this).addClass("now").siblings().removeClass("now");
+  var id = $(this).data("id");
+  renderSecond(id);
+})
